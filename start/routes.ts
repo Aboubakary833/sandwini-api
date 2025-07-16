fd397caa124a4e6dd118b257d94f70833ec8ebeb
Aboubakary833 <aboubakarycisse472@gmail.com>
2025-07-16 02:50:16 +0000

    chores: apply throttle middleware on routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,8 +1,10 @@
 import router from '@adonisjs/core/services/router'
 import authRoutes from './routes/auth.js'
+import { apiThrottle } from './limiter.ts'
 
 router
   .group(() => {
     authRoutes()
   })
   .prefix('api/v1')
+  .use(apiThrottle)
