4c17344b89e8ef76765754d824823a17868598c7
Aboubakary833 <aboubakarycisse472@gmail.com>
2025-07-21 10:58:31 +0000

    chores: removed unused code

--- app/policies/service_policy.ts
+++
@@ -1,6 +0,0 @@
-import User from '#models/user'
-import Service from '#models/service'
-import { BasePolicy } from '@adonisjs/bouncer'
-import type { AuthorizerResponse } from '@adonisjs/bouncer/types'
-
-export default class ServicePolicy extends BasePolicy {}
